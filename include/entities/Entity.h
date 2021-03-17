91aebf8de5b05e07800214769d272bfd2c04723c
Ryan Purse <ryanpurse@gmail.com>
2021-03-17 22:19:30 +0000

    More setup to the CMake structure. Add a base entity class (Nothing implemented).

---
+++ include/entities/Entity.h
@@ -0,0 +1,26 @@
+/**
+ * @file Entity.h
+ * @brief [fill in this secton]
+ * Project: A2McgRayTracer
+ * @author Ryan Purse
+ * @version 1.0.0
+ * Initial Version: 17/03/2021
+ */
+
+
+#ifndef A2MCGRAYTRACER_ENTITY_H
+#define A2MCGRAYTRACER_ENTITY_H
+
+
+/**
+ * [Description goes here.]
+ * @author Ryan Purse
+ * @version 1.0.0 [Initial Version: 17/03/2021]
+ */
+class Entity
+{
+
+};
+
+
+#endif //A2MCGRAYTRACER_ENTITY_H
---
+++ src/entities/Entity.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file Entity.cpp.cc
+ * @brief [fill in this secton]
+ * Project: A2McgRayTracer
+ * @author Ryan Purse
+ * @version 1.0.0
+ * Initial Version: 17/03/2021
+ */
+
+
+#include "Entity.h"
