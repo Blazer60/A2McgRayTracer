0a126a2819953ba30d5304b47fd3945582693007
Ryan Purse <ryanpurse@gmail.com>
2021-03-18 10:53:32 +0000

    Added Ray Tracer renderer.

---
+++ include/renderer/RayTracer.h
@@ -0,0 +1,30 @@
+/**
+ * @file RayTracer.h
+ * @brief The renderer the displays the world to the screen.
+ * Project: A2McgRayTracer
+ * @author Ryan Purse
+ * @version 1.0.0
+ * Initial Version: 18/03/2021
+ */
+
+
+#ifndef A2MCGRAYTRACER_RAYTRACER_H
+#define A2MCGRAYTRACER_RAYTRACER_H
+
+#include "Camera.h"
+#include "Sphere.h"
+
+#include <iostream>
+
+/**
+ * The renderer the displays the world to the screen.
+ * @author Ryan Purse
+ * @version 1.0.0 [Initial Version: 18/03/2021]
+ */
+class RayTracer
+{
+
+};
+
+
+#endif //A2MCGRAYTRACER_RAYTRACER_H
---
+++ src/renderer/RayTracer.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file RayTracer.cpp.cc
+ * @brief [fill in this secton]
+ * Project: A2McgRayTracer
+ * @author Ryan Purse
+ * @version 1.0.0
+ * Initial Version: 18/03/2021
+ */
+
+
+#include "RayTracer.h"
