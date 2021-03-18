90af96792645f4c47883ecf6fad059b8df2a974f
Ryan Purse <ryanpurse@gmail.com>
2021-03-18 09:09:00 +0000

    Added ray class with no implementation

---
+++ include/utilities/raycast/Ray.h
@@ -0,0 +1,46 @@
+/**
+ * @file Ray.h
+ * @brief Allows a ray to be constructed and fired into the world.
+ * The Ray class will provide information about a ray cast while the
+ * hitInfo struct will provide detail about the point of interception.
+ * Project: A2McgRayTracer
+ * @author Ryan Purse
+ * @version 1.0.0
+ * Initial Version: 18/03/2021
+ */
+
+
+#ifndef A2MCGRAYTRACER_RAY_H
+#define A2MCGRAYTRACER_RAY_H
+
+#include "glm.hpp"
+
+/**
+ * Hit information for when a ray has collided with an object in world space.
+ * @author Ryan Purse
+ * @version 1.0.0 [Initial Version: 18/03/2021]
+ */
+struct hitInfo
+{
+    bool hit;
+    glm::vec3 colour;
+    glm::vec3 hitNormal;
+};
+
+/**
+ * Holds information about a ray vector in world space.
+ * @author Ryan Purse
+ * @version 1.0.0 [Initial Version: 18/03/2021]
+ */
+class Ray
+{
+private:
+    glm::vec3 mPosition;
+    glm::vec3 mNormal;
+public:
+    Ray() = default;
+    Ray(const glm::vec3 &mPosition, const glm::vec3 &mNormal);
+};
+
+
+#endif //A2MCGRAYTRACER_RAY_H
---
+++ src/utilities/raycast/Ray.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file Ray.cpp.cc
+ * @brief [fill in this secton]
+ * Project: A2McgRayTracer
+ * @author Ryan Purse
+ * @version 1.0.0
+ * Initial Version: 18/03/2021
+ */
+
+
+#include "Ray.h"
+
+Ray::Ray(const glm::vec3 &mPosition, const glm::vec3 &mNormal) : mPosition(mPosition), mNormal(mNormal)
+{}
