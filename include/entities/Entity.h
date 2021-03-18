c4246b300e69fe0fe3608e72d1d15720243d9a55
Ryan Purse <ryanpurse@gmail.com>
2021-03-18 08:50:32 +0000

    Implement abstract class Entity. Allowed entities to use the vendor.

--- include/entities/Entity.h
+++ include/entities/Entity.h
@@ -1,6 +1,7 @@
 /**
  * @file Entity.h
- * @brief [fill in this secton]
+ * @brief An entity is an object the can live in world space.
+ * NOTE: Not all entities can be seen by the camera. @see Actors.h
  * Project: A2McgRayTracer
  * @author Ryan Purse
  * @version 1.0.0
@@ -11,15 +12,25 @@
 #ifndef A2MCGRAYTRACER_ENTITY_H
 #define A2MCGRAYTRACER_ENTITY_H
 
+#include "glm.hpp"
 
 /**
- * [Description goes here.]
+ * An entity is an object the can live in world space.
+ * NOTE: Not all entities can be seen by the camera. @see Actors.h
  * @author Ryan Purse
  * @version 1.0.0 [Initial Version: 17/03/2021]
  */
 class Entity
 {
+protected:
+    glm::vec3 mPosition;
+    glm::vec3 mRotation;  // The will need to change.
+    glm::vec3 mScale;
+public:
+    Entity();
+    Entity(const glm::vec3 &mPosition, const glm::vec3 &mRotation, const glm::vec3 &mScale);
 
+    virtual void update(float deltaTime) = 0;
 };
 
 
--- src/entities/Entity.cpp
+++ src/entities/Entity.cpp
@@ -7,5 +7,17 @@
  * Initial Version: 17/03/2021
  */
 
-
 #include "Entity.h"
+
+
+Entity::Entity(const glm::vec3 &mPosition, const glm::vec3 &mRotation, const glm::vec3 &mScale) :
+    mPosition(mPosition),
+    mRotation(mRotation),
+    mScale(mScale)
+{}
+
+Entity::Entity() :
+    mPosition(0),
+    mRotation(0),
+    mScale(0)
+{}
