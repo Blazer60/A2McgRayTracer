05255a5269c3dc1881de76a36ca82d0156776060
Ryan Purse <ryanpurse@gmail.com>
2021-03-30 11:08:49 +0100

    Recreated the 4 Balls scene with the new lighting variables

--- src/renderer/RayTracer.cpp
+++ src/renderer/RayTracer.cpp
@@ -46,12 +46,44 @@ RayTracer::RayTracer(const glm::ivec2 &mWindowSize) :
 //    mPhysicalObjects.push_back(s1);
 //    mEntities.push_back(s1);
 
-    actorLightingMaterial mat1{};
-    auto *s1 = new Sphere({0.f, 0.f, 6.f},
-                          mat1,
+    actorLightingMaterial grey({0.3f, 0.3f, 0.3f},
+                               {0.1f, 0.1f, 0.1f},
+                               glm::vec3(0.05f),
+                               128.f);
+
+    actorLightingMaterial red({1.f, 0.f, 0.f},
+                              {0.5f, 0.5f, 0.5f},
+                              glm::vec3(0.8f),
+                              64.f);
+
+    actorLightingMaterial blue({0.f, 0.f, 0.7f},
+                              {0.2f, 0.2f, 0.2f},
+                              glm::vec3(0.3f),
+                              128.f);
+
+    auto *s1 = new Sphere({0.f, 1.73f, 7.f},
+                          grey,
                           1);
     mPhysicalObjects.push_back(s1);
     mEntities.push_back(s1);
+
+    auto *s2 = new Sphere({-1.f, 0.f, 7.f},
+                          blue,
+                          1);
+    mPhysicalObjects.push_back(s2);
+    mEntities.push_back(s2);
+
+    auto *s3 = new Sphere({1.f, 0.f, 7.f},
+                          grey,
+                          1);
+    mPhysicalObjects.push_back(s3);
+    mEntities.push_back(s3);
+
+    auto *s4 = new Sphere({0.f, -1.73f, 7.f},
+                          red,
+                          1);
+    mPhysicalObjects.push_back(s4);
+    mEntities.push_back(s4);
 
 //    auto *s1 = new Sphere({0.f, 0.0f, 5.f},
 //                          {0.5f, 0.5f, 0.5f},
@@ -61,7 +93,7 @@ RayTracer::RayTracer(const glm::ivec2 &mWindowSize) :
 //    mEntities.push_back(s1);
 
     // Adding lights
-    auto *l = new DirectionalLight(glm::vec3(-0.4f, 0.5f, -1.f), glm::vec3(1), 1);
+    auto *l = new DirectionalLight(glm::vec3(-0.4f, 0.5f, -1.f), glm::vec3(1), 1.f);
     mLights.push_back(l);
     mEntities.push_back(l);
 }
@@ -161,12 +193,12 @@ glm::vec3 RayTracer::traceShadows(Ray &ray, hitInfo &hit)
     // Reflection Ray
     ray.mDirection = glm::reflect(ray.mDirection, hit.hitNormal);
     ray.mPosition = hit.hitPosition;
-    ray.mEnergy = ray.mEnergy * hit.material.specularIntensity;
+    ray.mEnergy = ray.mEnergy * hit.material.reflectivityIntensity;
 
     return  hit.material.ambientIntensity
             + hit.material.diffuseIntensity * diffuseColour
-            + hit.material.specularIntensity * glm::pow(specularColour, hit.material.specularIntensity);
-}
+            + hit.material.specularIntensity * glm::pow(specularColour, glm::vec3(hit.material.shininessConstant));
+} //
 
 hitInfo RayTracer::getHitInWorld(const Ray &ray)
 {
