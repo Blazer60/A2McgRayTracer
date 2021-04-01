9b14c463a45e98aa0f5045b0c723aa5ceff1a721
Ryan Purse <ryanpurse@gmail.com>
2021-04-01 19:17:34 +0100

    Re-inverted the z-plane again (I'm not sure which vector is wrong at this point).

--- src/entities/Camera.cpp
+++ src/entities/Camera.cpp
@@ -87,6 +87,9 @@ Ray Camera::generateSingleRay(const glm::ivec2 &pixelPos)
     nearPlane = mInvPrtMat * nearPlane;
     farPlane = mInvPrtMat * farPlane;
 
+    nearPlane.z *= -1;
+    farPlane.z *= -1;
+
     // Normalise the vector coords by their w component.
     nearPlane /= nearPlane.w;
     farPlane /= farPlane.w;
